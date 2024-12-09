1c0da2ddc460616d6f010efc2bb0f9a666385864
Vasilis Pappas <vasilis.pappas@monday-consulting.com>
2024-12-09 11:41:00 +0100

    fixes ts issues in store

--- src/themeStore.ts
+++ src/themeStore.ts
@@ -1,17 +1,21 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export const useThemeStore = create(
+interface ThemeStore {
+  selectedTheme: string;
+  themeDir: string;
+  setSelectedTheme: (selectedTheme: string) => void;
+  setThemeDir: (themeDir: string) => void;
+}
+
+export const useThemeStore = create<ThemeStore>()(
   persist(
     (set) => ({
       selectedTheme: 'san-diego',
       themeDir: '/src/fc-themes',
-      setSelectedTheme: (selectedTheme: string) =>
-        set({ selectedTheme }),
-      setThemeDir: (themeDir: string) => set({ themeDir }),
+      setSelectedTheme: (selectedTheme) => set({ selectedTheme }),
+      setThemeDir: (themeDir) => set({ themeDir }),
     }),
-    {
-      name: 'fc-theme-storage', // name of the item in the storage (must be unique)
-    }
+    { name: 'fc-theme-storage' }
   )
 );
