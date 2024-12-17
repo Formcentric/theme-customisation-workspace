dee638502b57ecf960eb0eea0577f5a8c67e1875
Vasilis Pappas <vasilis.pappas@monday-consulting.com>
2024-12-17 11:19:29 +0100

    fixes svg attributes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -262,9 +262,9 @@ const Sidebar = ({ selectedTheme, formOptions, handleThemeChange, handleFormChan
                         viewBox='0 0 24 24'
                         fill='none'
                         stroke='currentColor'
-                        stroke-width='2'
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                         className='lucide lucide-layout-grid'
                     >
                         <rect
