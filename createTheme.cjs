00f6fab999620443f875cdda8d62319d1eab942b
Vasilis Pappas <vasilis.pappas@monday-consulting.com>
2024-12-19 13:57:24 +0100

    aborts theme creation if theme dir already exists

--- createTheme.cjs
+++ createTheme.cjs
@@ -1,8 +1,25 @@
 const { execSync } = require('child_process')
 const path = require('path')
+const fs = require('fs')
+
+// Function to add colors to terminal output
+const COLORS = {
+    red: '\x1b[31m',
+    reset: '\x1b[0m',
+}
 
 function createTheme(themeName, targetDir) {
     const finalTargetDir = targetDir || `${themeName}-custom`
+    const themePath = path.resolve('src/themes', finalTargetDir) // Resolve the full path to the target directory
+
+    // Abort process if the theme dir already exists
+    if (fs.existsSync(themePath)) {
+        console.log(
+            `${COLORS.red}Error: The theme directory "src/themes/${finalTargetDir}" already exists!${COLORS.reset}`,
+        )
+        console.log('If you want to create a different theme, choose a unique name or delete the existing directory.')
+        process.exit(0)
+    }
 
     try {
         console.log(`Creating theme: ${themeName} at ${finalTargetDir}`)
