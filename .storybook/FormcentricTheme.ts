daf00f24649e4b7d83067362fc85aafe6159ae30
Nils Böhm <nils.boehm@monday-consulting.com>
2025-02-24 20:08:45 +0100

    FC-1822 add Formcentric theming to storybook

---
+++ .storybook/FormcentricTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming'
+
+export default create({
+    base: 'light',
+    brandTitle: 'Formcentric',
+    brandUrl: 'https://formcentric.com',
+    brandImage: 'assets/logo.svg',
+    brandTarget: '_self',
+})
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api'
+import FormcentricTheme from './FormcentricTheme'
+
+addons.setConfig({
+    theme: FormcentricTheme,
+})
