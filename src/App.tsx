59a6330f4be1b5347ff7a852b298ac225e73736a
Nils Böhm <nils.boehm@monday-consulting.com>
2025-11-06 19:50:56 +0700

    import formapp directy instead of dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import config from '../config/formcentric.config.js'
 import { generateUid, removeUid } from './helpers/uid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import '@formcentric/client/dist/formapp.js'
 
 declare global {
     interface Window {
@@ -219,7 +220,6 @@ function App() {
     const localFormDefinition = formDefinition ? formDefinition : localConfig.fcFormDefinition
 
     const commonProps = {
-        'data-fc-formapp-url': modulePath + '/formapp.js',
         'data-fc-theme-dir': themeDir,
         'data-fc-theme': removeUid(selectedTheme),
     }
